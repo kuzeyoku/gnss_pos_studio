366960b9eaeab0ed9dd694d76722a6c399b56e57
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2026-09-08 09:16:10 +0300

    chore: remove obsolete mind map documentation

--- backups/backup_before_html_components/Baslat_Web_Studio.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title GNSS Pos Web Studio - Local Server
-echo ========================================================
-echo GNSS Pos Web Studio v2.0 Baslatiliyor...
-echo ========================================================
-echo Tarayici aciliyor: http://localhost:8080
-start http://localhost:8080
-python -m http.server 8080
-pause
